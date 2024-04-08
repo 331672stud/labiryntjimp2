e8cf4fc7035f1ef7e930d80cafb4fc6c93a02a30
Karol Krukowski <01178546@pw.edu.pl>
2024-04-08 19:37:38 +0200

    getopt

--- Source/getopt.c
+++ Source/getopt.c
@@ -1,4 +1,12 @@
+#include "unistd.h"
 
 
-
-parsearg(argc, argv, optstring, filename, resultname)
+void parsearg(int argc, char **argv, char *optstring, char **filename, char **resultname){
+    int opt;
+    while((opt = getopt(argc, argv, optstring)) != -1){
+        switch(opt){
+            case 't':
+                *filename = optarg;
+        }
+    }
+}
