4b30241c61c969b46a2b75c36efc3a39802bf389
git <01187521@pw.edu.pl>
2024-04-11 20:28:24 +0200

    struktura grafu

--- Source/adjacencyMatrix.h
+++ Source/adjacencyMatrix.h
@@ -12,7 +12,8 @@ typedef struct cell{
 } cell_t; 
 
 typedef struct graph{
-    int numer;
+    int dirs; //w pierwszej komorce numer, dla kazdej kolejnej direction od niej: 0,1,2,3,4(wskazowki zegara od prawej)
+    int countbranch; //ile galezi z tego punktu grafu, do iterowania po next 
     struct graph *next;
 } graph_t
 
--- Source/matrixGraphConverter.c
+++ Source/matrixGraphConverter.c
@@ -2,7 +2,12 @@
 
 void conv2graph(cell_t **labirynt, graph_t *graf, int width, int height){
     //trzeba dodac zaczynanie od startu nie od 0
-    for(int i=0;i<)
+    for(int i=0;i<height;i++){
+        for(int j=0;j<width;j++){
+        
+
+        }
+    }
     free labirynt;
 }
 
