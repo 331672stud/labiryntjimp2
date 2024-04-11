daa93da011416f2a4c87d35447c908df8e15c888
git <01187521@pw.edu.pl>
2024-04-11 20:44:47 +0200

    current

--- Source/matrixGraphConverter.c
+++ Source/matrixGraphConverter.c
@@ -1,13 +1,15 @@
 #include "valconvert.h"
 
+void zalewanielabiryntu(cell_t **labirynt, graph_t *graf, int komorka){
+    //zaczynajac od startu, zgodnie z komorkami cell_t budowac graph_t
+        //graf skierowany, wiec po dodaniu przejscia z jednej komorki usuwany adres tej na ktorej jestesmy z tej drugiej
+        //jesli wiecej niz jedna alokacja pamieci na wskaznik next
+        //rekurencyjnie poruszac sie po cell_t zgodnie z komorkami ktore dodajemy do graph_t
+}
+
 void conv2graph(cell_t **labirynt, graph_t *graf, int width, int height){
     //trzeba dodac zaczynanie od startu nie od 0
-    for(int i=0;i<height;i++){
-        for(int j=0;j<width;j++){
-        
-
-        }
-    }
-    free labirynt;
+    int komorka; //adres startu
+    zalewanielabiryntu(labirynt, graf, komorka);
 }
 
--- Source/matrixGraphConverter.h
+++ Source/matrixGraphConverter.h
@@ -2,6 +2,8 @@
 #define CONVERT_H
 #include "adjacencyMatrix.h"
 
+void zalewanielabiryntu(cell_t **labirynt, graph_t *graf, int komorka);
+
 void conv2graph(cell_t **labirynt, graph_t *graf, int width, int height);
 
 #endif
