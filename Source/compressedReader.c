4b9d89aef72a37275793b71327a9cf96237f1e40
Karol Krukowski <01178546@pw.edu.pl>
2024-04-11 21:16:16 +0200

    czytanie bin

--- Source/compressedReader.c
+++ Source/compressedReader.c
@@ -22,8 +22,30 @@ void compRead(char *filename, graph_t *graf, cell_t **labirynt){
     int reserved;
     int counter;
     int solution_offset;
-    int separator;
+    int separator_id;
     int wall;
     int path;
+    int separator;
+    int value;
+    int count;
+
+    fread(file_id, (32 / 8), 1, plik);
+    fread(escape, (8 / 8), 1, plik);
+    fread(columns, (16 / 8), 1, plik);
+    fread(entry_x, (16 / 8), 1, plik);
+    fread(entry_y, (16 / 8), 1, plik);
+    fread(exit_x, (16 / 8), 1, plik);
+    fread(exit_y, (16 / 8), 1, plik);
+    fread(reserved, (96 / 8), 1, plik);
+    fread(counter, (32 / 8), 1, plik);
+    fread(solution_offset, (32 / 8), 1, plik);
+    fread(separator_id, (32 / 8), 1, plik);
+    fread(wall, (8 / 8), 1, plik);
+    fread(path, (8 / 8), 1, plik);
+    fread(separator, (8 / 8), 1, plik);
+    fread(value, (8 / 8), 1, plik);
+    fread(count, (8 / 8), 1, plik);
+
+    fclose(plik);
 
 }
