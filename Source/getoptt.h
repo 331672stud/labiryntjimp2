b7bfdbaa732aee72e725820de30b9fe26d97fb18
git <01187521@pw.edu.pl>
2024-04-14 15:08:32 +0200

    zmiana

--- Source/getoptt.h
+++ Source/getoptt.h
@@ -1,6 +1,6 @@
 #ifndef ARGREAD_H
 #define ARGREAD_H
 
-void parsearg(int argc, char **argv, char *optstring, char *filename, char *resultname);
+void parsearg(int argc, char **argv, char *optstring, char **filename, char **resultname);
 
 #endif
